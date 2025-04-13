43cae0b499a215f960736b72e4bd42ba43199ebb
borg0861 <borg0861@gmail.com>
2025-04-13 15:53:09 -0500

    added java and additional styling for expandable toc box

--- assets/js/ds-custom.js
+++ assets/js/ds-custom.js
@@ -20,3 +20,18 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+//toc-accordion
+var tocacc = document.getElementsByClassName("toc-button");
+var iacc;
+
+for (iacc = 0; iacc < acc.length; iacc++) {
+  tocacc[iacc].addEventListener("click", function() {
+    this.classList.toggle("toc-button-active");
+    var tocbox = document.getElementsByClassName("toc-box");
+    if (tocbox.style.display === "block") {
+    } else {
+      tocbox.style.display = "block";
+    }
+  });
+}
